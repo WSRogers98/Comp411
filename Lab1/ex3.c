25b2a32a833d1ef9adf07497579742a5f1bbb345
William Rogers <williamsrogers98@gmail.com>
2019-02-26 12:12:51 -0500

    Update ex3.c

--- Lab1/ex3.c
+++ Lab1/ex3.c
@@ -12,7 +12,7 @@ int main()
 
   a = PI *(r/2.54) *(r / 2.54);
   c = 2 *PI *(r/2.54);
-// this hurts my eyes
+
   printf("Circle's area is %3.2f (sq in).\n", a);
   printf("Its circumference is %3.2f (in).\n", c);
 	}
